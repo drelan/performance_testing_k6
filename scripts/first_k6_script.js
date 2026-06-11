73ee63876dd4a9991a3ece98616815ddb8cd52e6
Deepak Relan <derelan@gmail.com>
2026-06-11 02:47:50 -0700

    Add capability for HTML report generation

--- scripts/first_k6_script.js
+++ scripts/first_k6_script.js
@@ -1,7 +1,14 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export default function () {
     http.get('https://test.k6.io');
     sleep(1)      //adding sleep to mimic real world scenario for now/temporarily
+}
+
+export function handleSummary(data) {
+    return {
+        "report.html": htmlReport(data),
+    }
 }
